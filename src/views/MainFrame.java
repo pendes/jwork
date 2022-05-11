cbc72fc7614e72fcf15eb937d4ef0ca838b06a3e
Sallai Andras <andrewteki@gmail.com>
2022-05-11 09:02:19 +0200

    Added ScrollPane

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -1,6 +1,7 @@
 package views;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 
 import javax.swing.DefaultComboBoxModel;
@@ -9,6 +10,7 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 
 public class MainFrame extends JFrame {
@@ -18,6 +20,7 @@ public class MainFrame extends JFrame {
 
     public DefaultListModel<String> employeeModel;
     public JList<String> employeeList;
+    JScrollPane employeeScroll;
 
     public MainFrame() {
         this.setMainComponent();
@@ -27,22 +30,25 @@ public class MainFrame extends JFrame {
     private void setMainComponent() {
         this.rankModel = new DefaultComboBoxModel<>();
         this.rankCombo = new JComboBox<>(this.rankModel);
+        
 
         this.employeeModel = new DefaultListModel<>();
         this.employeeList = new JList<>(this.employeeModel);
-        
+        this.employeeScroll = new JScrollPane(this.employeeList);
+        this.employeeScroll.setPreferredSize(new Dimension(200, 200));
     }
     private void setMainPanel() {
         this.mainPanel = new JPanel();
         this.mainPanel.setLayout(new BorderLayout());
         this.mainPanel.add(this.rankCombo, BorderLayout.NORTH);
-        this.mainPanel.add(this.employeeList, BorderLayout.CENTER);
+        this.mainPanel.add(this.employeeScroll, BorderLayout.CENTER);
     }
     private void setMainFrame() {
         this.add(this.mainPanel);
         this.setLayout(new FlowLayout());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(300, 400);
+        // this.setSize(300, 400);
+        this.pack();
         this.setVisible(true);
     }
 }
